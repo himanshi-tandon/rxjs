130e175bb26c9cc80c7e6e626d39d8d7aec299c7
Suryansh <suryanshsrivastava9616@gmail.com>
2019-11-15 23:33:07 +0530

    Adding images

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,6 @@
-console.log('Hii');
-
-addItem('Hii');
-
-function addItem(data: any) {
-    var node = document.createElement('li');
-    var textNode = document.createTextNode(data);
-    node.appendChild(textNode);
-    document.getElementById('list').appendChild(node);
-}
+// RxJS v6+
+import { of } from 'rxjs';
+//emits any number of provided values in sequence
+const source = of(1, 2, 3, 4, 5);
+//output: 1,2,3,4,5
+const subscribe = source.subscribe(val => console.log(val));
